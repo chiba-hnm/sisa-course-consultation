5a4abef0e830e00595dd7ef6f32863f812442061
chiba <chiba.hnm@gmail.com>
2021-03-21 21:52:06 +0900

    edit_dbpath

--- models/models.py
+++ models/models.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'postgresql://postgres:@localhost/sample'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'postgresql://postgres:@localhost/sample'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
